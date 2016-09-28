2d73f7fa265fad0f7b031ab0bba455ae0e08ec4b
cjiang2 <cjiang2@ualberta.ca>
2016-09-27 18:06:02 -0600

    lab 4 changes

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,36 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cjiang2 on 9/27/16.
+ */
+public class TweetList {
+    List<Tweet> tweets = new ArrayList<Tweet>();
+    public void add(Tweet tweet) {
+
+        if (hasTweet(tweet)){
+            throw new IllegalArgumentException();
+        }
+
+        tweets.add(tweet);
+
+    }
+
+    public boolean hasTweet(Tweet tweet){
+        return tweets.contains(tweet);
+    }
+
+    public Tweet getTweet(int i){
+        return tweets.get(i);
+    }
+
+    public void delete(Tweet a){
+        tweets.remove(a);
+    }
+
+    public int getCount(){
+        return tweets.size();
+    }
+}
